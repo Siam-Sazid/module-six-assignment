9913d1b22e070675fb9b177fe3a7e223a2d55b79
Siam-Sazid <124497059+Siam-Sazid@users.noreply.github.com>
2023-06-12 22:44:09 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen() ,
+    );}
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Photo Gallery',
+      ),
+     centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+child: Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+children: [
+  Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Container(
+      child: Text('Welcome to my photo gallery',
+      style: TextStyle(fontSize: 20,wordSpacing: 2,fontWeight: FontWeight.bold, ),
+      ),
+    ),
+  ),
+
+  Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: TextField(
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.search,color: Colors.grey,size: 30,),
+        hintText: 'Search',
+        hintStyle: TextStyle(fontSize: 24,color: Colors.grey,),
+      border: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey,),
+      ),
+      ),
+
+    ),
+  ),
+
+  Wrap(
+children: [
+ /*
+ Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,),
+  Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,),
+  Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,),
+  Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,),
+  Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,),
+  Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,),*/
+
+
+//Elevated button
+  ElevatedButton(
+    onPressed: () {
+      print('Clicked on photo!');
+    },
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue, // Set the button background color
+    ),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,), // Replace with your image path
+        Positioned(
+          bottom: 8.0, // Adjust the position of the caption
+          child: Text(
+            'Caption',
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.grey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+
+  ElevatedButton(
+    onPressed: () {
+      print('Clicked on photo!');
+    },
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue, // Set the button background color
+    ),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,), // Replace with your image path
+        Positioned(
+          bottom: 8.0, // Adjust the position of the caption
+          child: Text(
+            'Caption',
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.grey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+
+  ElevatedButton(
+    onPressed: () {
+      print('Clicked on photo!');
+    },
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue, // Set the button background color
+    ),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,), // Replace with your image path
+        Positioned(
+          bottom: 8.0, // Adjust the position of the caption
+          child: Text(
+            'Caption',
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.grey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+
+  ElevatedButton(
+    onPressed: () {
+      print('Clicked on photo!');
+    },
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue, // Set the button background color
+    ),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,), // Replace with your image path
+        Positioned(
+          bottom: 8.0, // Adjust the position of the caption
+          child: Text(
+            'Caption',
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.grey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+
+  ElevatedButton(
+    onPressed: () {
+      print('Clicked on photo!');
+    },
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue, // Set the button background color
+    ),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,), // Replace with your image path
+        Positioned(
+          bottom: 8.0, // Adjust the position of the caption
+          child: Text(
+            'Caption',
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.grey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+
+  ElevatedButton(
+    onPressed: () {
+      print('Clicked on photo!');
+    },
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue, // Set the button background color
+    ),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',width: 100,height: 100,), // Replace with your image path
+        Positioned(
+          bottom: 8.0, // Adjust the position of the caption
+          child: Text(
+            'Caption',
+            style: TextStyle(
+
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              backgroundColor: Colors.grey,
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+
+
+
+
+
+
+
+
+
+],
+  )
+
+],
+
+  ),
+),
+
+
+
+
+      ),
+    );
+  }
+}
+
+
